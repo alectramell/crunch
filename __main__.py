ccbd93acb04170d4814349adbbd81ca2b2d145eb
alectramell <alectramell@gmail.com>
2014-07-08 02:42:27 -0700

    CRUNCH v1.0

---
+++ __main__.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("clear")
+
+print ("Copy crunch.py to your app directory, then run it as so: python crunch.py")
---
+++ crunch.py
@@ -0,0 +1,7 @@
+import os
+
+# Make sure that the crunch.py script is inside your package directory before you run it.. example: (myapp/crunch.py), and remember to make sure your package includes the __main__.py file.. example: (myapp/__main__.py)
+
+os.system("clear && python -m compileall __main__.py")
+
+os.system("clear")
